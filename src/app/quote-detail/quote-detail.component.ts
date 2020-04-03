8a80a29fa4319e60723579382122b51c0732eb5f
SaintieMonte10 <claremonte53@gmai.com>
2020-04-03 08:58:48 +0300

    added quote form

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit,Input,Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
+
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
-export class GoalDetailComponent implements OnInit {
+export class QuoteDetailComponent implements OnInit {
 
-  @Input() quote: Quote;
+  @Input() quote:Quote;
   @Output() isComplete = new EventEmitter<boolean>();
 
-  goalComplete(complete:boolean){
+  quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
+    
   }
+
   constructor() { }
 
   ngOnInit() {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,29 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
-  templateUrl:'./quote.component.html',
+  templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
 
-  quotes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks'),
-    new Quote(5,'Solve math homework','Damn Math'),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord'),
-  ];
-  toggleDetails(index){
+  public quotes: Quote[] = [
+    new Quote(1, "Opportunity", "Don't Wait for opportunity. Create it.", "Mburia", new Date(2019, 11, 1), 0, 0),
+    new Quote(2, "Perseverance", "If you want to see the rainbow you have to put up with the rain", "Skimankings", new Date(2019, 11, 1), 0, 0),
+    new Quote(3, "Technology", "Technology is a useful servant but a dangerous master.", "Hammerton", new Date(2019, 11, 1), 0, 0),
+    new Quote(4, "Determination", " “The grass is always greener when you water it”.", "Angelo", new Date(2019, 11, 1), 0, 0),
+  ]
+  
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    this.quotes.unshift(quote)
+  }
+
+  toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  completeGoal(isComplete, index){
+
+  quoteDelete(isComplete, index) {
     if (isComplete) {
-      this.quotes.splice(index,1);
+      let toDelete = confirm(`You are about to delete ${this.quotes[index].name}, Would you like to continue?`)
+
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
     }
   }
+
+  numberOfLikes : number=0;
+  numberOfDislikes =0;
+
+  likeButtonClick(i) {
+    this.quotes[i].numberOfLikes++;
+  }
+
+  
+
+  dislikeButtonClick(i) {
+    this.quotes[i].numberOfDislikes++;
+  }
+
+  preNum:number
+  lastNum:number
+  counter:number
+
+  highestUpvote(){
+    this.preNum = 0
+    this.lastNum = 0
+
+    for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+      this.lastNum = this.quotes[this.counter].numberOfLikes;
+      if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+    }
+    return  this.preNum
+  }
+
+
   constructor() { }
 
   ngOnInit() {
